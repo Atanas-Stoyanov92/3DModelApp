ce596f4a857e88ede55ea6a50f3493596ea4b8ee
Atanas Stoyanov <130588261+Atanas-Stoyanov92@users.noreply.github.com>
2024-11-10 01:24:53 +0200

    css issue

--- Djangoadvanceproject/settings.py
+++ Djangoadvanceproject/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-&_wy9%7lja_$$3nj+9znma0!2!r3os$(sa0a(8nd_#(nlk4)ct
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['threedmodelapp.onrender.com']
+ALLOWED_HOSTS = ['threedmodelapp.onrender.com','127.0.0.1']
 
 # Application definition
 
@@ -40,6 +40,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'Djangoadvanceproject.urls'
@@ -108,11 +109,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/staticfiles/'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles_collected'
 
 STATICFILES_DIRS = (
     BASE_DIR / 'staticfiles',
 )
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
